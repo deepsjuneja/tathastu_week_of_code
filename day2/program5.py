b10e8d4311ec55c28093e5b2078acfeb4b724631
deepsjuneja <52399007+deepsjuneja@users.noreply.github.com>
2020-06-02 03:02:44 +0530

    Create program5.py

---
+++ day2/program5.py
@@ -0,0 +1,15 @@
+n = int(input("Enter the value of n: "))
+
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print(i, end=" ")
+        if j != i:
+            print("*", end=" ")
+    print()
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i, end=" ")
+        if j != i:
+            print("*", end=" ")
+    print()
